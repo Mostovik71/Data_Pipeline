120daf46cc995781ad9403493c462eace8f96202
Mostovik71 <mostovik71@gmail.com>
2024-02-10 13:44:57 +0300

    added debezium connector

--- kafka/kafka_consumer.py
+++ kafka/kafka_consumer.py
@@ -1,11 +1,26 @@
-import json
 from kafka import KafkaConsumer
+from avro.schema import parse_schema
+from avro.io import DatumReader, BinaryDecoder
+import io
+
+schema_av = parse_schema(open("C:/Users/mosto/Desktop/DE_Pipeline/habits_schema.avsc", "rb").read())
+# reader = DatumReader(schema_av)
+
+
+
+def decode(msg_value):
+    message_bytes = io.BytesIO(msg_value)
+#    message_bytes.seek(2)
+    decoder = BinaryDecoder(message_bytes)
+    record = schema_av.read(decoder)
+    #event_dict = reader.read(decoder)
+    return record
 
 if __name__ == '__main__':
-    consumer = KafkaConsumer('test-topic',
-                             bootstrap_servers='localhost:9094',
-                             auto_offset_reset='earliest'
+    consumer = KafkaConsumer("postgres.public.reports_data",
+                             bootstrap_servers=["localhost:29092"]
                              )
-    
+                             
     for message in consumer:
-        print(json.loads(message.value))
+        data = decode(message.value)
+        print(data)
