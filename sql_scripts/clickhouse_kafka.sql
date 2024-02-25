b0952ecc045e221a3e3b8da74733040a750db61d
Mostovik71 <mostovik71@gmail.com>
2024-02-25 18:26:35 +0300

    configured clickhouse connect

--- sql_scripts/clickhouse_kafka.sql
+++ sql_scripts/clickhouse_kafka.sql
@@ -1,24 +1,15 @@
 CREATE TABLE habit.clckhs_reports_data 
 (
  user_id String, 
- pari_report String,
- date String,
- status String,
- reason String,
- date_waiting String
+ pari_report String
 )
 ENGINE = MergeTree
 PRIMARY KEY (user_id)
 
-
 CREATE TABLE habit.clckhs_reports_data_kafka 
 (
- user_id String, 
- pari_report String,
- date String,
- status String,
- reason String,
- date_waiting String
+ `after.user_id` String, 
+ `after.pari_report` String
 )
 ENGINE = Kafka
 SETTINGS kafka_broker_list = 'kafka:9092',
@@ -26,14 +17,11 @@ SETTINGS kafka_broker_list = 'kafka:9092',
        kafka_group_name = 'group1',
        kafka_format='JSONEachRow';
        
-
- CREATE MATERIALIZED VIEW habit.consumer TO habit.clckhs_reports_data
-    AS SELECT user_id, pari_report, date, status, reason, date_waiting
+CREATE MATERIALIZED VIEW habit.consumer TO habit.clckhs_reports_data
+    AS SELECT after.user_id as user_id, after.pari_report as pari_report
     FROM habit.clckhs_reports_data_kafka;
-   
-   
-SELECT *
-FROM habit.clckhs_reports_data
+    
+select * from habit.clckhs_reports_data
       
       
       
